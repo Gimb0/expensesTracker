bbd23bba214f0ef54ada260a3588738e45f64ce2
bradley.semlitzky <bradley.semlitzky@fdmgroup.com>
2020-08-12 14:47:41 +1000

    Trying to test main App

---
+++ frontend/src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+// import App from './App';
+// import Expenses from './components/Expenses';
+import expectExport from 'expect';
+
+test('<App> has <Expenses>', () => {
+	expect(true).toBeTruthy();
+});
