8841dc91d7c41f187a89c569ba8ce915f0477849
bradley.semlitzky <bradley.semlitzky@fdmgroup.com>
2020-08-12 14:47:25 +1000

    Initial Main App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Expenses from './components/Expenses'
+
+class App extends Component {
+	render() {
+		return (
+			<Expenses />
+		);
+  	}
+}
+
+export default App;
