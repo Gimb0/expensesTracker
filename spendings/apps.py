bfdf857a364361e0617afb0f6755ede2f23df9b9
bradley.semlitzky <bradley.semlitzky@fdmgroup.com>
2020-08-19 21:12:48 +1000

    clean up

--- spendings/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class SpendingsConfig(AppConfig):
-    name = 'spendings'
--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-// import App from './App';
-// import Expenses from './components/Expenses';
-import expectExport from 'expect';
-
-test('<App> has <Expenses>', () => {
-	expect(true).toBeTruthy();
-});
