8dc236250862dfa0a0de0f913385c30341056de7
Diligenc3 <bradgimbo@gmail.com>
2020-12-07 23:34:12 +0000

    Change API URLs for Authentication

--- backend/financeSite/urls.py
+++ backend/financeSite/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
+# from rest_framework import routers
 from spendings import views
 
-router = routers.DefaultRouter()
-router.register(r'spendings/expenses', views.ExpensesView, 'expenses')
-router.register(r'spendings/categories', views.CategoryView, 'categories')
+# router = routers.DefaultRouter()
+# router.register(r'spendings/expenses', views.ExpensesView, 'expenses')
+# router.register(r'spendings/categories', views.CategoryView, 'categories')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    
+    path('api/', include('authentication.urls')),
 ]
