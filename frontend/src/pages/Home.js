ba521a164e9d35030f35df57bc85f412b2471a21
Bradley Semlitzky <gimbo@Bradleys-MacBook-Air.local>
2020-12-09 09:20:05 +1100

    Create a home page

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+
+        return (
+            <main className="content">
+                <h1 className="text-white text-uppercase text-center my-4">Finance Manager</h1>
+            </main>
+        )
+    }
+}
