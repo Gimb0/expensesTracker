a08e39668021326b3a14f7d98a3872e882c5e769
Bradley Semlitzky <gimbo@Bradleys-MacBook-Air.local>
2020-12-09 09:20:41 +1100

    Configure App with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,42 @@
-import React, { Component } from "react";
-import Expenses from './components/Expenses'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
 
-class App extends Component {
-	render() {
-		return (
-			<Expenses />
-		);
-  	}
+import Home from './pages/Home'
+import Expenses from './pages/Expenses'
+import Login from './pages/Login'
+
+export default function App() {
+return (
+	<Router>
+	<div>
+		<Navbar color="dark" dark expand="md">
+			<NavbarBrand href="/">FinanceManager</NavbarBrand>
+			<Nav className="container-fluid">
+				<NavItem>
+					<NavLink href="/">Home</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink href="/expenses">Expenses</NavLink>
+				</NavItem>
+				<NavItem className="ml-auto">
+					<NavLink href="/login">Login</NavLink>
+				</NavItem>
+			</Nav>
+		</Navbar>
+		
+		<Switch>
+			<Route path="/login">
+				<Login />
+			</Route>
+			<Route path="/expenses">
+				<Expenses />
+			</Route>
+			<Route path="/">
+				<Home />
+			</Route>
+		</Switch>
+	</div>
+	</Router>
+);
 }
-
-export default App;
