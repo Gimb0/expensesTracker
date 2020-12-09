8dfab22cc4b922d53ba6db2d9b91e1ebf2f03e5f
Bradley Semlitzky <gimbo@Bradleys-MacBook-Air.local>
2020-12-09 17:21:04 +1100

    Simple home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -13,6 +13,7 @@ export default class Home extends Component {
         return (
             <main className="content">
                 <h1 className="text-white text-uppercase text-center my-4">Finance Manager</h1>
+                <p className="text-white text-center my-2">Welcome to your personal finance toolkit</p>
             </main>
         )
     }
